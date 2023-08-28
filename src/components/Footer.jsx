16173d1d838eddaf3640f6f938d568dc93a6dde6
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-28 20:00:41 +0530

    Footer responsiveness done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,18 @@ import { GithubIcon } from "./Icons";
 
 const Footer = () => {
   return (
-    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light">
-      <Layout className="py-8 flex items-center justify-between">
+    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light sm:text-base">
+      <Layout className="py-6 flex items-center justify-between lg:flex-col  lg:py-8 sm:text-sm">
         <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
-        <div className="flex">
-          <GithubIcon className="!w-6 mr-2" />
+        <div className="flex my-2 whitespace-nowrap">
+          <GithubIcon className="!w-6 mr-2  md:hidden" />
           <Link href="https://github.com/ucantbe-jagmeet">
             @ucantbe-jagmeet
           </Link>
         </div>
-        <Link href="/">Say Hello</Link>
+        <Link href="/" className="md:hidden inline-block">
+          Say Hello
+        </Link>
       </Layout>
     </footer>
   );
